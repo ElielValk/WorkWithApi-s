dc3af7e739ded6881b65876fe3257b56c0b296ce
ElielValk <112205041+ElielValk@users.noreply.github.com>
2022-08-27 14:42:29 +0300

    routing system

---
+++ src/Components/Quotes.js
@@ -0,0 +1,11 @@
+import {Link} from 'react-router-dom';
+
+
+export function Quotes(){
+    return (
+        <div style={{textAlign : "center"}}>
+        <h1 >What Quotes do you want today?</h1>
+        
+        </div>
+    )
+}
